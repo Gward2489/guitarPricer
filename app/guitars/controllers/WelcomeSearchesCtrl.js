7d80c0481c3e7cf5cab249f1f252c161878bad9e
Gward2489 <gww8804@gmail.com>
2017-12-06 12:26:09 -0600

    working avergae price search for basic search. put firebase keys in gitignore

--- app/guitars/controllers/WelcomeSearchesCtrl.js
+++ app/guitars/controllers/WelcomeSearchesCtrl.js
@@ -2,9 +2,13 @@ angular
 .module("GuitarPricerApp")
 .controller("WelcomeSearchesCtrl", function ($scope, GuitarFactory) {
 
-    $scope.getBasicPrice = function () {
-        console.log("hey")
-        GuitarFactory.basicSearch()
+    
+
+    $scope.getBasicPrice = function (searchInput) {
+        let removedDoublespaces = searchInput.replace(/  /g, "+")
+        let convertedSearch = removedDoublespaces.replace(/ /g, "+")
+        GuitarFactory.basicSearch(convertedSearch)
+        console.log(convertedSearch)
     }
 
 })
--- app/guitars/factories/GuitarFactory.js
+++ app/guitars/factories/GuitarFactory.js
@@ -4,17 +4,29 @@ angular
 
 
 
-    return Object.create(null, {
-        "basicSearch":{
-            value: function () {
-                let url = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=GarrettW-KellyBlu-PRD-e132041a0-8df2cdd9&RESPONSE-DATA-FORMAT=XML&categoryId=33034&itemFilter.name=SoldItemsOnly&itemFilter.value=true&REST-PAYLOAD&keywords=2014+Gibson+Les+Paul+Traditional`
-                let trustedUrl = $sce.trustAsResourceUrl(url)
-                return $http.jsonp(trustedUrl, 
-                    {jsonpCallbackParam: 'callback'})
-                    .then(response => {
-                    console.log(response)
-                }) 
+return Object.create(null, {
+    "basicSearch":{
+        value: function (userSearch) {
+            let url = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=GarrettW-KellyBlu-PRD-e132041a0-8df2cdd9&RESPONSE-DATA-FORMAT=XML&categoryId=33034&itemFilter.name=SoldItemsOnly&itemFilter.value=true&REST-PAYLOAD&keywords=${userSearch}`
+            let trustedUrl = $sce.trustAsResourceUrl(url)
+            return $http.jsonp(trustedUrl, 
+                {jsonpCallbackParam: 'callback'})
+                .then(response => {
+                    let guitarPricesArray = response.data.findCompletedItemsResponse[0].searchResult[0].item.map(guitar => {
+                    let guitarPrice = parseFloat(guitar.sellingStatus[0].convertedCurrentPrice[0].__value__)
+                       return guitarPrice
+                    })
 
+                    let divisor = guitarPricesArray.length
+
+                    let dividend = guitarPricesArray.reduce(function (accumulator, currentValue) {
+                        return accumulator + currentValue })
+
+                    let avgPrice = (dividend/divisor).toFixed(2)
+
+                    console.log(avgPrice)
+
+                })  
             }
         }
     })
